1997b08195af9c98402c8946844329f4e89cd5ad
BlockFather <sean@blockfather.xyz>
2022-10-18 14:15:48 -0500

    Update Connected.tsx

--- components/Connected.tsx
+++ components/Connected.tsx
@@ -1,26 +1,41 @@
-import { Container, VStack, Heading, Button, Text, HStack, Image, Box, Flex, Wrap, WrapItem } from '@chakra-ui/react'
-import { FC, MouseEventHandler, useCallback, useEffect, useMemo, useState } from 'react'
-import { PublicKey } from '@solana/web3.js'
-import { useConnection, useWallet } from '@solana/wallet-adapter-react'
 import {
+    Button,
+    Container,
+    Heading,
+    VStack,
+    Text,
+    HStack,
+    Image,
+  } from "@chakra-ui/react"
+  import {
+    FC,
+    MouseEventHandler,
+    useCallback,
+    useEffect,
+    useMemo,
+    useState,
+  } from "react"
+  import { PublicKey } from "@solana/web3.js"
+  import { useConnection, useWallet } from "@solana/wallet-adapter-react"
+  import {
     Metaplex,
     walletAdapterIdentity,
     CandyMachine,
-} from "@metaplex-foundation/js"
-import { useRouter } from 'next/router'
-
-const Connected: FC = () => {
-
+  } from "@metaplex-foundation/js"
+  import { useRouter } from "next/router"
+  
+  const Connected: FC = () => {
     const { connection } = useConnection()
     const walletAdapter = useWallet()
     const [candyMachine, setCandyMachine] = useState<CandyMachine>()
     const [isMinting, setIsMinting] = useState(false)
-
+  
     const metaplex = useMemo(() => {
-        return Metaplex.make(connection).use(walletAdapterIdentity(walletAdapter))
+      return Metaplex.make(connection).use(walletAdapterIdentity(walletAdapter))
     }, [connection, walletAdapter])
-
+  
     useEffect(() => {
+<<<<<<< Updated upstream
         if (!metaplex) return
 
         metaplex
@@ -40,9 +55,52 @@ const Connected: FC = () => {
             })
     }, [metaplex])
 
+=======
+      handleInitialLoad()
+    }, [metaplex, walletAdapter])
+  
+    const handleInitialLoad = useCallback(async () => {
+      if (!metaplex || !walletAdapter.publicKey) return
+  
+      try {
+        const candyMachine = await metaplex
+          .candyMachines()
+          .findByAddress({
+            address: new PublicKey(
+              process.env.NEXT_PUBLIC_CANDY_MACHINE_ADDRESS ?? ""
+            ),
+          })
+          .run()
+  
+        const nfts = await metaplex
+          .nfts()
+          .findAllByOwner({ owner: walletAdapter.publicKey })
+          .run()
+  
+        const nft = nfts.find(
+          (nft) =>
+            nft.collection?.address.toBase58() ===
+            candyMachine.collectionMintAddress?.toBase58()
+        )
+  
+        if (nft) {
+          const metadata = await (await fetch(nft.uri)).json()
+          router.push(
+            `/stake?mint=${nft.mintAddress}&imageSrc=${metadata?.image}`
+          )
+        }
+  
+        setCandyMachine(candyMachine)
+      } catch (error) {
+        alert(error)
+      }
+    }, [metaplex, walletAdapter])
+  
+>>>>>>> Stashed changes
     const router = useRouter()
-
+  
     const handleClick: MouseEventHandler<HTMLButtonElement> = useCallback(
+<<<<<<< Updated upstream
         async (event) => {
             if (event.defaultPrevented) return
 
@@ -63,8 +121,30 @@ const Connected: FC = () => {
             }
         },
         [metaplex, walletAdapter, candyMachine, router]
+=======
+      async (event) => {
+        if (event.defaultPrevented) return
+  
+        if (!walletAdapter.connected || !candyMachine) {
+          return
+        }
+  
+        try {
+          setIsMinting(true)
+          const nft = await metaplex.candyMachines().mint({ candyMachine }).run()
+  
+          console.log(nft)
+          router.push(`/newMint?mint=${nft.nft.address.toBase58()}`)
+        } catch (error) {
+          alert(error)
+        } finally {
+          setIsMinting(false)
+        }
+      },
+      [metaplex, walletAdapter, candyMachine]
+>>>>>>> Stashed changes
     )
-
+  
     return (
         <VStack spacing={10}>
             <Container>
@@ -107,13 +187,16 @@ const Connected: FC = () => {
                   
                
                 </Box>
-            
+
 
             
         </VStack>
 
 
-    )
+)
 }
 
 export default Connected
+
+
+
